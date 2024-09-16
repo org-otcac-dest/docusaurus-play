d53649bc594104c6f642429f077317a570ff12de
Kyriakos Akriotis <akriotis.kyriakos@gmail.com>
2024-09-16 15:45:00 +0200

    changes to config.ts Test PR Workflow /3 #5

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site PR3',
+  title: 'My Site PR5',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -11,7 +11,7 @@ const config: Config = {
   url: 'https://akyriako.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/' + process.env.REACT_APP_DOCUSAURUS_BASE_URL,
+  baseUrl: process.env.REACT_APP_DOCUSAURUS_BASE_URL,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
